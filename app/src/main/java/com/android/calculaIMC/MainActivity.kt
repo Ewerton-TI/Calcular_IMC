c0e4369e1624b4ec1c9efc528532ebfd907eb323
Ewerton-TI <ewertonoliveiramartins@hotmail.com>
2021-08-27 15:17:13 -0300

    Calculating IMC and Demonstrating Ranking

--- app/src/main/java/com/android/calculaIMC/MainActivity.kt
+++ app/src/main/java/com/android/calculaIMC/MainActivity.kt
@@ -19,7 +19,33 @@ class MainActivity : AppCompatActivity() {
             titleTXT?.text = text
         }
         alturaEDT.doAfterTextChanged { text ->
-            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, text.toString(), Toast.LENGTH_LONG).show()
+        }
+        calcularBTN.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun  calcularIMC(pesoEDT: String, alturaEDT:String){
+        val peso = pesoEDT.toFloatOrNull()
+        val altura = alturaEDT.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é \n%.2f".format(imc)
+            resultView.text = grauIMC(imc)
+        }
+    }
+
+    private fun grauIMC(gImc: Float): String{
+        return when(gImc){
+            in 0.1..17.0 -> "Muito abaixo do peso."
+            in 17.1..18.49 -> "Abaixo do peso."
+            in 18.5..24.99 -> "Peso normal."
+            in 25.0..29.99 ->  "Acima do peso."
+            in 30.0..34.99 -> "Obesidade I."
+            in 35.0..39.99 -> "Obesidade II(severa)."
+            else -> "Obesidade III(mórbida)."
         }
     }
 }
