8f7cf3d8cfbfbe477b1bafcf3608064c5a792fc7
Ewerton-TI <ewertonoliveiramartins@hotmail.com>
2021-08-27 11:31:46 -0300

    initial layout completed

---
+++ app/src/main/java/com/android/calculaIMC/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.android.calculaIMC
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
